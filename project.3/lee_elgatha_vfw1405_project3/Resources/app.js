56682d6ce0b4570076074017bc1113a2fdbb57ac
elgatha <appleelgatha@fullsail.edu>
2014-05-24 21:43:55 -0400

    Lee_Elgatha_VFW1405_Project 3

---
+++ project.3/lee_elgatha_vfw1405_project3/Resources/app.js
@@ -0,0 +1,49 @@
+//Elgatha Lee
+//VFW 1405
+//Project 3
+
+var mainWindow = Ti.UI.createWindow();
+
+var subWindow = Ti.UI.createWindow({
+	title: "Take A Look Inside",
+	modal: true,
+	backgroundColor: "#000000"
+});
+
+var subWindowSecond = Ti.UI.createWindow({
+	title: "My Life",
+	modal: true,
+	backgroundColor: "#000000"
+});
+
+var navWindow = Ti.UI.iOS.createNavigationWindow({
+	window: subWindow
+});
+
+var navWindowSecond = Ti.UI.iOS.createNavigationWindow({
+	window: subWindowSecond
+});
+
+var viewScrollView = Ti.UI.createScrollView({
+	contentHeight: Ti.Platform.displayCaps.platformHeight,
+	contentWidth: Ti.Platform.displayCaps.platformWidth,
+	showVerticalScrollIndicator: true,
+	layout: "horizontal"
+});
+
+var table = Ti.UI.createTableView({
+	style: Ti.UI.iPhone.TableViewStyle.GROUPED
+});
+
+var myButton = Ti.UI.createButton({
+	title: "My Life",
+	backgroundColor: "#ccc",
+	borderWidth: 4,
+	borderColor: "#fff",
+	borderRadius: 0,
+	height: 50,
+	width: 175,
+	top: 150,
+});
+
+var myLifeFile = require('life');
